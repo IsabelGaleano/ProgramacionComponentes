eaea512dd87e51c1bbf478a28ddbfc9de70b968c
Isabel <isagaleano9@gmail.com>
2022-04-07 12:09:47 -0600

    add structure test

---
+++ Examenes/examen2/auditorias/src/test/java/com/cenfotec/examen/auditorias/AuditoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.examen.auditorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
