f2769024f0dbe73bf3ddbfeb774a95ff8705b134
Isabel <isagaleano9@gmail.com>
2022-04-07 21:35:26 -0600

    Add registro y listado de auditores

--- Examenes/examen2/examen/src/main/java/com/cenfotec/examen/controller/AuditorController.java
+++ Examenes/examen2/examen/src/main/java/com/cenfotec/examen/controller/AuditorController.java
@@ -6,14 +6,32 @@ import com.cenfotec.examen.service.JournalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class AuditorController {
     @Autowired
     AuditorService auditorService;
+
     @RequestMapping("/listadoAuditores")
     public String index(Model model){
         model.addAttribute("auditor", auditorService.getAll());
         return "listadoAuditores";
     }
+
+    @RequestMapping(value = "/registroAuditores", method = RequestMethod.GET)
+    public String navegarPaginaInsertar(Model model){
+        model.addAttribute(new Auditor());
+        return "registroAuditores";
+    }
+
+    @RequestMapping(value = "/registroAuditores", method = RequestMethod.POST)
+    public String accionInsertar(Auditor auditor, BindingResult result, Model model){
+        auditorService.saveAuditor(auditor);
+        return "index";
+    }
+
+
 }
--- Examenes/examen2/examen/src/main/java/com/cenfotec/examen/controller/JournalController.java
+++ Examenes/examen2/examen/src/main/java/com/cenfotec/examen/controller/JournalController.java
@@ -16,4 +16,6 @@ public class JournalController {
         return "index";
     }
 
+
+
 }
